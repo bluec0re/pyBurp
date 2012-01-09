b11a962db272d15d91b0de8c4a64b60e2f396e4a
Timo Schmid <tschmid@ernw.de>
2012-01-09 11:40:26 +0100

    error handling

--- burp.sh
+++ burp.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
 BURP_PATH=
+BURP_JAR=burpsuite_pro_v1.4.05.jar
 JYTHON_PATH=
 PLUGIN_PATH=
-CP="$PLUGIN_PATH/burp_python.jar:$JYTHON_PATH/jython.jar:$BURP_PATH/burpsuite_pro_v1.4.05.jar"
+CP="$PLUGIN_PATH/burp_python.jar:$JYTHON_PATH/jython.jar:$BURP_PATH/$BURP_JAR"
+
+if [[ ! -d "$JYTHON_PATH/Lib" || ! -f "$JYTHON_PATH/jython.jar" ]]
+then
+    echo -e "[ \e[31m-\e[0m ] Invalid jython path \e[36m$JYTHON_PATH\e[0m"
+    exit 1
+fi
+
+if [[ ! -d "$BURP_PATH" || ! -f "$BURP_PATH/$BURP_JAR" ]]
+then
+    echo -e "[ \e[31m-\e[0m ] Invalid burp path \e[36m$BURP_PATH\e[0m \e[93mOR\e[0m"
+    echo -e "[ \e[31m-\e[0m ] Invalid burp jar \e[36m$BURP_JAR\e[0m"
+    exit 1
+fi
+
+if [[ ! -f "$PLUGIN_PATH/CmdlinePlugin.py" ]]
+then
+    echo -e "[ \e[31m-\e[0m ] CmdlinePlugin.py not found in \e[36m$PLUGIN_PATH\e[0m"
+fi
 
 java -Xmx1536m -classpath $CP burp.StartBurp --python-home=$JYTHON_PATH/Lib $@ |& while read line
 do
